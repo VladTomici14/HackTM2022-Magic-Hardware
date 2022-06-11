56b742f7b3fc371fe738aaef3f6b3a391a87f9de
Victor TOPORAN <victor.toporan@gmail.com>
2022-06-11 18:47:58 +0300

    Repo cleanup

--- HackTM2022-Magic-Hardware.ino
+++ HackTM2022-Magic-Hardware.ino
@@ -2,21 +2,10 @@
 
 int pwm;
 
-<<<<<<< HEAD
 //Motor shiled I2C Address: 0x30
 //PWM frequency: 1000Hz(1kHz)
 Motor M1(0x30,_MOTOR_A, 1000);//Motor A
 Motor M2(0x30,_MOTOR_B, 1000);//Motor B
-=======
-void startServo() {
-    servo.attach(servoSignal, 500, 2500);
-}
-
-void stop_servo(){
-    servo.detach();
-}
->>>>>>> eb87c532e5da92cf7b1a41968327cbb51905c3b6
-
 
 void setup() {
     Serial.begin(9600);
