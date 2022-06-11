3dac76b15eb645f20441fc8c6cf42ad9eb0d909a
VladTomici14 <vladtomici@gmail.com>
2022-06-11 13:16:57 +0300

    Edited the servo control functions.

--- servo.ino
+++ servo.ino
@@ -26,39 +26,48 @@ void setup() {
     delay(1000);
 }
 
-void rotateServoDegrees(Servo servo, int degrees){
+void rotateServoDegrees(Servo servo, int degrees, int time){
     /**
      * @param: servo - The servo motor that you want to use.
      * @param: degrees - The number of position of [0, 180] degrees you want to adjust the servo motor.
+     * @param: time - Rotation time.
      */
 
     // --------- making the rotation -------
-    int position = 500;
-    if(degrees != 0) {
-        position = (2500 * degrees) / 180;
-    } if(degrees == 90){
-        position = 1500;
-    }
-    servo.writeMicroseconds(position);
+//    int position = 500;
+//    if(degrees != 0) {
+//        position = (2500 * degrees) / 180;
+//    } if(degrees == 90){
+//        position = 1500;
+//    }
+    servo.write(degrees);
 
     // ------- printing the rotation values to the serial monitor ------
-    char message[64], degrees_2_char[10], position_2_char[10];
-    sprintf(degrees_2_char, "%d", degrees);
-    sprintf(position_2_char, "%d", position);
+//    char message[64], degrees_2_char[10], position_2_char[10];
+//    sprintf(degrees_2_char, "%d", degrees);
+//    sprintf(position_2_char, "%d", position);
+//
+//    strcat(message, "Rotation of ");
+//    strcat(message, degrees_2_char);
+//    strcat(message, " = ");
+//    strcat(message, position_2_char);
+//    strcat(message, "µs \n");
 
-    strcat(message, "Rotation of ");
-    strcat(message, degrees_2_char);
-    strcat(message, " = ");
-    strcat(message, position_2_char);
-    strcat(message, "µs \n");
-
-    Serial.write(message);
-    delay(1500);
+//    Serial.write(message);
+    delay(time);
 }
 
 void loop() {
-    rotateServoDegrees(servo, 0);
-    rotateServoDegrees(servo, 90);
-    rotateServoDegrees(servo, 180);
+    rotateServoDegrees(servo, 0, 1500);
+    rotateServoDegrees(servo, 90, 1500);
+    rotateServoDegrees(servo, 180, 1500);
+    rotateServoDegrees(servo, 270, 1500);
+    rotateServoDegrees(servo, 360, 1500);
+
 }
 
+void determine_stop_location(Servo servo, int time){
+    for(int i = 0; i <= 180; i++){
+        rotateServoDegrees(servo, i, time);
+    }
+}
