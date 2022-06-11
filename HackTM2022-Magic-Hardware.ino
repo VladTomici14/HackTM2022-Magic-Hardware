68b023fd9cf4dec9be2eeed43b653c6460e99177
Victor TOPORAN <victor.toporan@gmail.com>
2022-06-11 18:42:01 +0300

    Changes servo lib

--- HackTM2022-Magic-Hardware.ino
+++ HackTM2022-Magic-Hardware.ino
@@ -1,9 +1,13 @@
-#include <Servo.h>
+#include "WEMOS_Motor.h"
 
-Servo servo;
-int servoPower = D3;
-int servoSignal = D8;
+int pwm;
 
+<<<<<<< HEAD
+//Motor shiled I2C Address: 0x30
+//PWM frequency: 1000Hz(1kHz)
+Motor M1(0x30,_MOTOR_A, 1000);//Motor A
+Motor M2(0x30,_MOTOR_B, 1000);//Motor B
+=======
 void startServo() {
     servo.attach(servoSignal, 500, 2500);
 }
@@ -11,33 +15,20 @@ void startServo() {
 void stop_servo(){
     servo.detach();
 }
+>>>>>>> eb87c532e5da92cf7b1a41968327cbb51905c3b6
 
-void setDirection(int dir) {
-    if (dir == 1) {
-        servo.write(0);
-        return;
-    }
-    if (dir == -1) {
-        servo.write(180);
-        return;
-    }
-    stopServo();
-}
 
 void setup() {
-    servo.attach(servoSignal, 500, 2500);
     Serial.begin(9600);
-    Serial.println("Start");
-    delay(1000);
 }
 
 void loop() {
-    startServo();
-    setDirection(1);
+    M1.setmotor(_CW, 100);
+    delay(3000);
+    M1.setmotor(_STOP);
     delay(1000);
-    setDirection(-1);
+    M1.setmotor(_CCW, 100);
+    delay(3000);
+    M1.setmotor(_STOP);
     delay(1000);
-    setDirection(0);
-    delay(1000);
-    Serial.println("Loop");
 }
